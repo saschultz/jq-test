8dbc53edb4a73ed4ac8314467212117aa1bdda57
Sara Schultz and Shane Stafford <shane@>
2017-02-14 10:22:40 -0800

    add more images, jquery functions, and styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#initially-hidden").toggle();
-    $("#initially-showing").toggle();
+    $("#walrus-showing").hide();
+    $("#walrus-hidden").show();
+  });
+  $(".clickable2").click(function() {
+    $("#walrus-hidden").hide();
+    $("#giraffe-hidden").show();
+  });
+  $(".clickable3").click(function() {
+    $("#giraffe-hidden").hide();
+    $("#monkey-hidden").show();
+  });
+  $(".clickable4").click(function() {
+    $("#monkey-hidden").hide();
+    $("#walrus-showing").show();
   });
 });
